aa41e3cdc85981e6e1c8b7ea64c7dbc4c11bf99e
Alex Nikiforov <unleash211@gmail.com>
2025-08-15 21:14:02 +0400

    trying selenoid connect1

--- src/test/java/com/nikiforov/TestBase.java
+++ src/test/java/com/nikiforov/TestBase.java
@@ -8,18 +8,18 @@ public class TestBase {
 @BeforeAll
     static void beforeAll() {
     Configuration.browser = "chrome";
-    Configuration.browserVersion = "128.0";
+//    Configuration.browserVersion = "128.0";
     Configuration.browserSize = "1920x1080";
     Configuration.pageLoadStrategy = "eager";
 //    Configuration.timeout = 6000;
-    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--incognito");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--disable-gpu");
-    options.addArguments("--disable-extensions");
+//    options.addArguments("--no-sandbox");
+//    options.addArguments("--disable-dev-shm-usage");
+//    options.addArguments("--disable-gpu");
+//    options.addArguments("--disable-extensions");
     Configuration.browserCapabilities = options;
     }
 }
