d934fb77142f7dc7d17cca0b7a0b148f0420e0f3
Alex Nikiforov <unleash211@gmail.com>
2025-08-16 21:43:16 +0400

    test changes

--- src/test/java/com/nikiforov/tests/FindVacancyTests.java
+++ src/test/java/com/nikiforov/tests/FindVacancyTests.java
@@ -13,26 +13,26 @@ import org.junit.jupiter.params.provider.CsvSource;
 @ExtendWith(LoggingExtension.class)
 public class FindVacancyTests extends TestBase {
 
-    MainPage mainPage = new MainPage();
-    VacanciesByCityPage vacanciesPage = new VacanciesByCityPage();
-    BeginCareerPage beginCareerPage = new BeginCareerPage();
-
     @ParameterizedTest
     @CsvSource({"Казань, Java", "Новосибирск, PHP"})
     void succesfullFindVacancyTest(String cityName, String jobName) {
-        mainPage.openPage()
+        new MainPage()
+                .openPage()
                 .setCity(cityName)
                 .setJob(jobName)
                 .clickFindButton();
-        vacanciesPage.checkVacancies(jobName);
+        new VacanciesByCityPage()
+                .checkVacancies(jobName);
     }
 
     @ParameterizedTest
     @CsvSource({"Казань, Совкомбанк Технологии, Младший аналитик", "Томск, Совкомбанк, Специалист"})
     void succesfullFindVacancyBeginCareerTest(String cityName, String companyName, String jobName) {
-        mainPage.openPage()
+        new MainPage()
+                .openPage()
                 .clickBeginCareerButton();
-        beginCareerPage.clickTraineeButton()
+        new BeginCareerPage()
+                .clickTraineeButton()
                 .setCity(cityName)
                 .setCompany(companyName)
                 .clickOk()
